8911045722aa2b28ece48d12d4f4992bce5cf900
Alejandro Rey <alejandro.rey@aircury.com>
2025-02-26 23:25:37 +0100

    Connector | Register data update command

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,27 @@
+use clap::{Args, Parser, Subcommand};
+
+#[derive(Parser)]
+#[command(version, about, long_about = None)]
+#[command(propagate_version = true)]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Subcommand)]
+enum Commands {
+    DataUpdate(DataUpdateInput),
+}
+
+#[derive(Args)]
+struct DataUpdateInput {}
+
 fn main() {
-    println!("Hello, world!");
+    let cli = Cli::parse();
+
+    match &cli.command {
+        Commands::DataUpdate(_input) => {
+            println!("Connector data update command executed successfully!");
+        }
+    }
 }
